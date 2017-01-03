2c3ed87df525589866435c712bb48e782274f19a
cubaeurokatmic <cubaeurokatmic@gmail.com>
2017-01-03 14:30:26 +0100

    Update

--- skin.py
+++ skin.py
@@ -575,12 +575,7 @@ def loadSingleSkinData(desktop, skin, path_prefix):
 			try:
 				name = get("name")
 				value = get("value")
-				if name.find('Font') != -1:
-					font = value.split(";")
-					if isinstance(font, list) and len(font) == 2:
-						parameters[name] = (str(font[0]), int(font[1]))
-				else:
-					parameters[name] = map(int, value.split(","))
+				parameters[name] = map(int, value.split(","))
 			except Exception, ex:
 				print "[SKIN] bad parameter", ex
 
