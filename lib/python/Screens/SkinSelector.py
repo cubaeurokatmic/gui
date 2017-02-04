90d535b17f20a1244be81a1720154b00164317b1
cubaeurokatmic <cubaeurokatmic@gmail.com>
2017-02-04 18:47:10 +0100

    Fix change skin

--- lib/python/Screens/SkinSelector.py
+++ lib/python/Screens/SkinSelector.py
@@ -20,8 +20,6 @@ class SkinSelectorBase:
 			self.skinlist.append(self.DEFAULTSKIN)
 		if self.PICONSKINXML and os.path.exists(os.path.join(self.root, self.PICONSKINXML)):
 			self.skinlist.append(self.PICONDEFAULTSKIN)
-		if self.ALTERNATESKINXML and os.path.exists(os.path.join(self.root, self.ALTERNATESKINXML)):
-			self.skinlist.append(self.ALTERNATESKIN)
 		for root, dirs, files in os.walk(self.root, followlinks=True):
 			for subdir in dirs:
 				dir = os.path.join(root,subdir)
@@ -76,19 +74,19 @@ class SkinSelectorBase:
 
 	def ok(self):
 		if self["SkinList"].getCurrent() == self.DEFAULTSKIN:
-			self.skinfile = ""
-			self.skinfile = os.path.join(self.skinfile, self.SKINXML)
+			skinfile = ""
+			skinfile = os.path.join(skinfile, self.SKINXML)
 		elif self["SkinList"].getCurrent() == self.PICONDEFAULTSKIN:
-			self.skinfile = ""
-			self.skinfile = os.path.join(self.skinfile, self.PICONSKINXML)
-		elif self["SkinList"].getCurrent() == self.ALTERNATESKIN:
-			self.skinfile = ""
-			self.skinfile = os.path.join(self.skinfile, self.ALTERNATESKINXML)
+			skinfile = ""
+			skinfile = os.path.join(skinfile, self.PICONSKINXML)
 		else:
-			self.skinfile = self["SkinList"].getCurrent()
-			self.skinfile = os.path.join(self.skinfile, self.SKINXML)
+			skinfile = self["SkinList"].getCurrent()
+			skinfile = os.path.join(skinfile, self.SKINXML)
 
-		print "Skinselector: Selected Skin: "+self.root+self.skinfile
+		print "Skinselector: Selected Skin: "+self.root+skinfile
+		self.config.value = skinfile
+		self.config.save()
+		configfile.save()
 		restartbox = self.session.openWithCallback(self.restartGUI,MessageBox,_("GUI needs a restart to apply a new skin\nDo you want to restart the GUI now?"), MessageBox.TYPE_YESNO)
 		restartbox.setTitle(_("Restart GUI now?"))
 
@@ -119,9 +117,6 @@ class SkinSelectorBase:
 		elif self["SkinList"].getCurrent() == self.PICONDEFAULTSKIN:
 			pngpath = "."
 			pngpath = os.path.join(os.path.join(self.root, pngpath), "piconprev.png")
-		elif self["SkinList"].getCurrent() == self.ALTERNATESKIN:
-			pngpath = "."
-			pngpath = os.path.join(os.path.join(self.root, pngpath), "alternate.png")
 		else:
 			pngpath = self["SkinList"].getCurrent()
 			try:
@@ -143,11 +138,9 @@ class SkinSelectorBase:
 
 class SkinSelector(Screen, SkinSelectorBase):
 	SKINXML = "skin.xml"
-	DEFAULTSKIN = _("< Default >")
+	DEFAULTSKIN = "< Default >"
 	PICONSKINXML = None
 	PICONDEFAULTSKIN = None
-	ALTERNATESKINXML = None
-	ALTERNATESKIN = None
 
 	skinlist = []
 	root = os.path.join(eEnv.resolve("${datadir}"),"enigma2")
@@ -161,11 +154,9 @@ class SkinSelector(Screen, SkinSelectorBase):
 
 class LcdSkinSelector(Screen, SkinSelectorBase):
 	SKINXML = "skin_display.xml"
-	DEFAULTSKIN = _("< Default >")
+	DEFAULTSKIN = "< Default >"
 	PICONSKINXML = "skin_display_picon.xml"
-	PICONDEFAULTSKIN = _("< Default with Picon >")
-	ALTERNATESKINXML = "skin_display_alternate.xml"
-	ALTERNATESKIN = _("< Alternate Skin >")
+	PICONDEFAULTSKIN = "< Default with Picon >"
 
 	skinlist = []
 	root = os.path.join(eEnv.resolve("${datadir}"),"enigma2/display/")
