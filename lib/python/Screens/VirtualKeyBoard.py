2892c07c786f7b590ddf89f60608d9ca2cb19864
cubaeurokatmic <cubaeurokatmic@gmail.com>
2017-06-26 19:54:09 +0200

    change language

--- lib/python/Screens/VirtualKeyBoard.py
+++ lib/python/Screens/VirtualKeyBoard.py
@@ -301,7 +301,7 @@ class VirtualKeyBoard(Screen):
 				[u">", u"Z", u"X", u"C", u"V", u"B", u"N", u"M", u";", u":", u"_", u"CLEAR"],
 				[u"SHIFT", u"SPACE", u"OK", u"LEFT", u"RIGHT", u"~"]]
 			self.lang = 'en_US'
-			self.nextLang = 'de_DE'
+			self.nextLang = 'it_IT'
 		self["country"].setText(self.lang)
 
 	def virtualKeyBoardEntryComponent(self, keys):
